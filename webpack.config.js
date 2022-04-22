a2345627175a874d56347cd26c2236cbcc3530fc
mmaracic <marijo.maracic@gmail.com>
2022-04-22 18:34:45 +0200

    Added webpack loging; removed typescript compiling flags contained in strict=true

--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,9 @@ module.exports = {
     resolve: {
         extensions: ['.js', '.ts', '.tsx']
     },
+    stats: {
+        preset: "detailed"
+    },
   plugins: [
     new ForkTsCheckerWebpackPlugin({
       typescript: {
